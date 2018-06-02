c9f81e046d8f991651afcbbfc53f8fc590f0956a
Christopher Stanley <cssthewriter@gmail.com>
2018-06-02 10:06:11 -0400

    controller paths

--- controllers/controller.js
+++ controllers/controller.js
@@ -9,9 +9,9 @@ var shopping_list = require("../models/shopping_list.js"),
 
 // Create all our routes and set up logic within those routes where required.
 
-// get users
+// Get users //
 router.get("/", function(req, res) {
-  cat.all(function(data) {
+  users.all(function(data) {
     var hbsObject = {
       users: data
     };
@@ -20,15 +20,61 @@ router.get("/", function(req, res) {
   });
 });
 
-// create a user
+// Create a user //
 router.post("/api/users", function(req, res) {
-    // return results
+    // Return results // 
 });
 
-// update a user
+// Update a user //
 router.put("/api/users/:id", function(req, res) {
-// update info
+// Update info //
 });
 
-// Export routes for server.js to use.
+// ------------------------------------------------------------------------- //
+
+// Get recipes //
+router.get("/", function(req, res) {
+  saved_recipes.all(function(data) {
+    var hbsObject = {
+      saved_recipes: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+// Create a recipe //
+router.post("/api/saved_recipes", function(req, res) {
+    // Return results // 
+});
+
+// Update a recipe //
+router.put("/api/saved_recipes/:id", function(req, res) {
+// Update info // 
+});
+
+// ------------------------------------------------------------------------- //
+
+// Get shopping list //
+router.get("/", function(req, res) {
+  shopping_list.all(function(data) {
+    var hbsObject = {
+      shopping_list: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+// Create a shopping list //
+router.post("/api/shopping_list", function(req, res) {
+    // Return results //
+});
+
+// Update a shopping list //
+router.put("/api/shopping_list/:id", function(req, res) {
+// Update info // 
+});
+
+// Export routes for server.js to use //
 module.exports = router;
