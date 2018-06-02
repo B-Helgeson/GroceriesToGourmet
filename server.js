437308cdec70ba4d5e386135dbd7c4c230f83775
B-Helgeson <brandonstevenhelgeson@gmail.com>
2018-06-02 09:11:20 -0400

    updated server

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
+    // constants for the server
 const   express = require("express"),
         bodyParser = require("body-parser"),
         app = express(),
         PORT = process.env.PORT || 3000,
         exphbs = require("express-handlebars"),
-        routes = require("./controllers/controller.js"),
-        db = require("./models");
+        routes = require("./controllers/controller.js");
+        // db = require("./models");
 
     // app use functionality
-    app.use(express.static("public"));
-    app.use(bodyParser.urlencoded({ extended: true }));
-    app.use(bodyParser.json());
-    app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-    app.set("view engine", "handlebars");
-    app.use(routes); 
+        app.use(express.static("public"));
+        app.use(bodyParser.urlencoded({ extended: true }));
+        app.use(bodyParser.json());
+        app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+        app.set("view engine", "handlebars");
+        app.use(routes); 
 
-app.listen(PORT, function() {
-  console.log("App now listening at localhost:" + PORT);
-});
+    // starts the server
+        app.listen(PORT, function() {
+        console.log("App now listening at localhost:" + PORT);
+        });
 
 // If Using Sequelize... 
 // db.sequelize
