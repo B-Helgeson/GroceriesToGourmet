54a55f10c6a81ae287b1717af20f4b12be203e5a
B-Helgeson <brandonstevenhelgeson@gmail.com>
2018-06-02 17:04:14 -0400

    updated server file and added base html files

--- server.js
+++ server.js
@@ -3,24 +3,16 @@ const   express = require("express"),
         bodyParser = require("body-parser"),
         app = express(),
         PORT = process.env.PORT || 3000,
-        // exphbs = require("express-handlebars"),
-        routes = require("./controllers/controller.js");
-        // db = require("./models");
+        db = require("./models");
 
     // app use functionality
         app.use(express.static("public"));
         app.use(bodyParser.urlencoded({ extended: true }));
         app.use(bodyParser.json());
-        // app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-        // app.set("view engine", "handlebars");
         app.use(routes); 
 
-    // starts the server (disabled for sequelize below)
-        // app.listen(PORT, function() {
-        // console.log("App now listening at localhost:" + PORT);
-        // });
 
-    // If Using Sequelize... 
+    // Starts the Sequelize Server 
         db.sequelize
           .sync( { force: true} )
           .then(() => app.listen(PORT,() => console.log("App listening on PORT " + PORT )))
