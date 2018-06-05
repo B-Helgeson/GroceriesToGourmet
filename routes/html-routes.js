ece0f93986c43cf5b717b6866c22a265d706900e
Tom Knight <tomnite@gmail.com>
2018-06-05 19:21:32 -0400

    changed res.render to res.sendFile

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -7,19 +7,23 @@ module.exports = function(app) {
   // Each of the below routes handles the html pages the user gets sent to.
 
     app.get("/", function(req, res) {
-        res.render("index")
+        // res.render("index")
+        res.sendFile( path.resolve(__dirname, "../public/index.html"))
     })
 
     app.get("/loggedin", function(req, res) {
-        res.render("home")
+        // res.render("home")
+        res.sendFile( path.resolve(__dirname, "../public/home.html"))
     })
 
     app.get("/shoppinglist", function(req, res) {
-        res.render("shoppingList")
+        //res.render("shoppingList")
+        res.sendFile( path.resolve(__dirname, "../public/shoppingList.html"))
     })
 
     app.get("/useredit", function(req, res) {
-        res.render("user")
+        //res.render("user")
+        res.sendFile( path.resolve(__dirname, "../public/user.html"))
     })
 
 }
