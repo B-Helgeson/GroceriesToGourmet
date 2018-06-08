f8e6fe3766aa5021d6eae484e908bca5f57444da
Tom Knight <tomnite@gmail.com>
2018-06-08 00:18:25 -0400

    Added all passport code

--- controllers/authController.js
+++ controllers/authController.js
@@ -10,3 +10,14 @@ exports.signin = function(req, res) {
   res.sendFile( path.join(__dirname, "../public/index.html"));
 
 };
+exports.dashboard = function(req,res){
+
+	res.render('dashboard'); 
+
+};
+exports.logout = function(req,res){
+
+  req.session.destroy(function(err) {
+  res.redirect('/');
+  })
+};
--- routes/auth.js
+++ routes/auth.js
@@ -5,27 +5,20 @@ module.exports = function(app,passport) {
     app.get('/signup', authController.signup);
     app.get('/signin', authController.signin);
  
-    app.post('/signup', passport.authenticate('local-signup',  {            successRedirect: '/dashboard', failureRedirect: '/signup'}
+    app.post('/signup', passport.authenticate('local-signup',  {            successRedirect: '/home', failureRedirect: '/signup'}
     ));
 
+    app.get('/dashboard', isLoggedIn, authController.dashboard);
 
-// app.get('/dashboard',isLoggedIn, controller.dashboard);
+    app.get('/logout',authController.logout);
 
+    app.post('/signin', passport.authenticate('local-signin',  {            successRedirect: '/dashboard',
+        failureRedirect: '/signin'}
+    ));
 
-// app.get('/logout',controller.logout);
-
-
-// app.post('/signin', passport.authenticate('local-signin',  { successRedirect: '/dashboard',
-//     failureRedirect: '/signin'}
-//     ));
-
-
-// function isLoggedIn(req, res, next) {
-// if (req.isAuthenticated())
-// return next();
-
-// res.redirect('/signin');
-// }
-
-
+    function isLoggedIn(req, res, next) {
+        if (req.isAuthenticated())
+        return next();
+        res.redirect('/signin');
+    }
 }
